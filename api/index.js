15f975076972e8e74d6fb8575015ad38ab073c0e
vivaxy <xyxuye2007@126.com>
2019-09-30 15:16:12 +0800

    feat(api): :sparkles: add npm registry proxy

--- api/index.js
+++ api/index.js
@@ -2,7 +2,7 @@
  * @since 2019-09-30 02:11
  * @author vivaxy
  */
-const { URL } = require('url');
+const { parse } = require('url');
 
 function getCurrentCli(ua) {
   if (ua.includes('yarn')) {
@@ -12,21 +12,24 @@ function getCurrentCli(ua) {
 }
 
 module.exports = function(req, res) {
-  const reqURL = new URL(req.url);
+  const reqURL = parse(req.url);
   const [, targetCli, encodedRegistry, ...paths] = reqURL.path.split('/');
-  if (targetCli && encodedRegistry) {
+  let registry = decodeURIComponent(encodedRegistry);
+  if (['npm', 'yarn'].includes(targetCli) && encodedRegistry && parse(registry).host) {
     const currentCli = getCurrentCli(req.headers['user-agent']);
     if (currentCli !== targetCli) {
       res.statusCode = 403;
-      res.statusMessage = `Use ${targetCli} for installing.`;
+      res.send(`Use ${targetCli} for installing.`);
     } else {
-      res.statusCode = 302;
-      let registry = decodeURIComponent(encodedRegistry);
       if (!registry.endsWith('/')) {
         registry += '/';
       }
+      res.statusCode = 302;
       res.setHeader('Location', registry + paths.join(paths));
       res.send(null);
     }
+  } else {
+    res.statusCode = 404;
+    res.send('Hello World!');
   }
 };
